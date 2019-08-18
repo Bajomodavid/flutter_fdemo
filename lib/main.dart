d50d0906662dfe9fe6e51331d035436556303333
Bajomo David Mayowa <bajomomayowa@gmail.com>
2019-08-18 18:14:52 +0100

    Second commit

--- lib/main.dart
+++ lib/main.dart
@@ -80,7 +80,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Hello World',
+              'Hello World!',
             ),
           ],
         ),
